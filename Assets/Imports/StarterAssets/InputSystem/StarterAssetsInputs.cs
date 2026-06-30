72721d08eb1109fe93cf0c66abcaf7f36c9af7b9
maxweillll <2985780092@qq.com>
2026-06-30 23:10:10 +0800

    导入了第一人称控制器报，修改inputsystem为invokunityevents

---
+++ Assets/Imports/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+    public class StarterAssetsInputs : MonoBehaviour
+    {
+        [Header("Character Input Values")]
+        public Vector2 move;
+        public Vector2 look;
+        public bool jump;
+        public bool sprint;
+
+        [Header("Movement Settings")]
+        public bool analogMovement;
+
+        [Header("Mouse Cursor Settings")]
+        public bool cursorLocked = true;
+        public bool cursorInputForLook = true;
+
+#if ENABLE_INPUT_SYSTEM
+        // ========== 原有 Send Messages 方法，保留 ==========
+        public void OnMove(InputValue value)
+        {
+            MoveInput(value.Get<Vector2>());
+        }
+
+        public void OnLook(InputValue value)
+        {
+            if (cursorInputForLook)
+            {
+                LookInput(value.Get<Vector2>());
+            }
+        }
+
+        public void OnJump(InputValue value)
+        {
+            JumpInput(value.isPressed);
+        }
+
+        public void OnSprint(InputValue value)
+        {
+            SprintInput(value.isPressed);
+        }
+
+        // ========== Invoke Unity Events 适配方法 ==========
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            MoveInput(context.ReadValue<Vector2>());
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            if (cursorInputForLook)
+            {
+                LookInput(context.ReadValue<Vector2>());
+            }
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            JumpInput(context.ReadValueAsButton());  // ← 改这里
+        }
+
+        public void OnSprint(InputAction.CallbackContext context)
+        {
+            SprintInput(context.ReadValueAsButton());  // ← 改这里
+        }
+#endif
+
+        public void MoveInput(Vector2 newMoveDirection)
+        {
+            move = newMoveDirection;
+        }
+
+        public void LookInput(Vector2 newLookDirection)
+        {
+            look = newLookDirection;
+        }
+
+        public void JumpInput(bool newJumpState)
+        {
+            jump = newJumpState;
+        }
+
+        public void SprintInput(bool newSprintState)
+        {
+            sprint = newSprintState;
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            SetCursorState(cursorLocked);
+        }
+
+        private void SetCursorState(bool newState)
+        {
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+    }
+}
