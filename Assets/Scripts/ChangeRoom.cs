cfb4c8af97fead0b1469c2cbe46e0ad0e0e9b315
maxweillll <2985780092@qq.com>
2026-06-30 23:55:27 +0800

    添加场景切换（进入房间）功能

--- Assets/Scripts/ChangeRoom.cs
+++ Assets/Scripts/ChangeRoom.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class ChangeRoom : MonoBehaviour
 {
+    public GameObject F_tip;
+    public bool inRoom;
+    public bool canInRoom;
     // Start is called before the first frame update
     void Start()
     {
-        
+        F_tip.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inRoom && canInRoom)
+        {
+            Debug.Log("切换场景");
+            SceneManager.LoadScene("GameOutRoom");
+        }
+    }
+
+    public void GetInRoomButton(InputAction.CallbackContext context)
+    {
+        inRoom = context.ReadValueAsButton();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("按F进入");
+        F_tip.SetActive(true);
+        canInRoom = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        F_tip.SetActive(false);
+        canInRoom = false;
     }
 }
---
+++ Assets/Scripts/PlayerPersistent.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPersistent : MonoBehaviour
+{
+    private static PlayerPersistent instance;
+
+    void Awake()
+    {
+        // 单例：如果已存在就销毁新的，保证全局只有一个玩家
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    // 切换场景后，把玩家放到新场景的出生点
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        GameObject spawnPoint = GameObject.FindWithTag("SpawnPoint");
+        if (spawnPoint != null)
+        {
+            // 直接改位置，不用改父级（DontDestroyOnLoad 的对象没有场景父级）
+            transform.position = spawnPoint.transform.position;
+            transform.rotation = spawnPoint.transform.rotation;
+        }
+    }
+}
